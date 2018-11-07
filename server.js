4f418f971c3d4589126bf717b49922bca374c143
shivendra nath Misra <30shivam@gmail.com>
2018-11-07 14:01:11 +0530

    prepare files for build and deploy

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
